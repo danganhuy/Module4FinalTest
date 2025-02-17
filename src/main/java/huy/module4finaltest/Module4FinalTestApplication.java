c09ec8df87ebc40cc117c65970346d3500edafb5
AnHuy <92672519+danganhuy@users.noreply.github.com>
2025-02-17 09:09:19 +0700

    init

---
+++ src/main/java/huy/module4finaltest/Module4FinalTestApplication.java
@@ -0,0 +1,13 @@
+package huy.module4finaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module4FinalTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module4FinalTestApplication.class, args);
+    }
+
+}
